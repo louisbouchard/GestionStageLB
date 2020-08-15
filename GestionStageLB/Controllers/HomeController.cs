33c130514895fde10d4c4cc61be04c26dc3feef5
LouisYNot <profcegep@hotmail.com>
2020-08-15 19:24:15 -0400

    MonCommentaire

--- GestionStageLB/Controllers/HomeController.cs
+++ GestionStageLB/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using GestionStageLB.Models;
 
 namespace GestionStageLB.Controllers
 {
+
     // Et j'aoute celui ci d'un autre poste de travail
     public class HomeController : Controller
     {
